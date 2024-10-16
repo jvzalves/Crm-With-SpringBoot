51bd257960e4ec64a8ebb9be63069951d04785b0
João Vitor <jvitor145@outlook.com>
2024-10-16 20:08:52 -0300

    Projeto criado

---
+++ src/main/java/com/jvzalves/crm/CrmWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.jvzalves.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmWithSpringBootApplication.class, args);
+	}
+
+}
